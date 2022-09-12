620e652752b996aa957e69651ef524a0f80c5e00
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-09-12 13:39:33 -0500

    string concatenation

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ print("200 is a number")
 # arithmetic opertion
 print(20*24*60)
 
+# string concatenation
+print("20 day are " + str(50) + " minutes" )
+
+print(f"20 day are  {50} minutes")
 
 
-
-
-
+`
 
 
 
