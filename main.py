9a7317c92eb28a41f36b224ec52da000732885fb
Iyanuoluwa-vic <adeyeriiyanu@gmail.com>
2022-09-12 13:46:16 -0500

    variable

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ print("20 day are " + str(50) + " minutes" )
 print(f"20 day are  {20*24*60*60} seconds")
 
 
+# variable
+calulateTotal= 24*60*60
+
+print("calculate the total is " + str(calulateTotal))
 
 
 
